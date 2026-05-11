d8a46b558e829032f36ce50aa6ff9701ef2de6d6
DewaSRY <sdewa6645@gmail.com>
2026-05-11 08:01:35 +0700

    update: setup web api

---
+++ apps/webportal/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@netlify/nuxt',
+    '@nuxthub/core',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/i18n'
+  ]
+})
